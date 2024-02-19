1184f59b4c8d5396cb86927e85833dc76ac9a7dd
Luis Torres <luis.i6nacio@gmail.com>
2024-02-19 16:37:03 -0300

    add unistyles basic setup

---
+++ style/themes.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  colors: {
+    typography: "#000000",
+    background: "#ffffff",
+  },
+}
+
+export const darkTheme = {
+  colors: {
+    typography: "#ffffff",
+    background: "#000000",
+  },
+}
