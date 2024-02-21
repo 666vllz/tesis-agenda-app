6cb08d838504f96459df68f913f3c8745c1b456c
Luis Torres <luis.i6nacio@gmail.com>
2024-02-21 16:11:24 -0300

    add new colors

--- style/themes.js
+++ style/themes.js
@@ -2,7 +2,14 @@ export const lightTheme = {
   colors: {
     primary: "#3685ff",
     backgroundPrimary: "#ffffff",
+    backgroundSecondary: "#1c1c1c",
+    backgroundWarning: "#ffcdce",
     textPrimary: "#000000",
+    textSecondary: "#d6d6d6",
+    textTertiary: "#868686",
+    textWarning: "#e5484d",
+    border: "#0000001f",
+    borderWarning: "#eb8e90",
   },
 }
 
@@ -10,6 +17,13 @@ export const darkTheme = {
   colors: {
     primary: "#3685ff",
     backgroundPrimary: "#161616",
+    backgroundSecondary: "#1c1c1c",
+    backgroundWarning: "#611623",
     textPrimary: "#f6f6f6",
+    textSecondary: "#d6d6d6",
+    textTertiary: "#868686",
+    textWarning: "#e5484d",
+    border: "#ffffff1f",
+    borderWarning: "#b54548",
   },
 }
